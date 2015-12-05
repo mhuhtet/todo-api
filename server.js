eba6ba1acb8915da525171836d3ea37481f43b5a
Mhu Htet <mhuhtet@artstepmm.com>
2015-12-05 19:36:55 +0630

    Refactor API with underscore

---
+++ server.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var _ = require('underscore');
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json())
+
+app.get('/', function (req, res) {
+    res.send('Todo API Root')
+});
+
+app.get('/todos', function(req, res){
+   res.json(todos) 
+});
+
+
+app.get('/todos/:id', function(req, res){
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+
+    
+    if(matchedTodo){
+        res.json(matchedTodo);
+    }else {
+        res.status(404).send();
+    }
+    
+});
+
+
+app.post('/todos', function (req, res) {
+   var body = _.pick(req.body, 'description', 'completed');
+    
+   if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+       return res.status(400).send();
+   }
+   body.description = body.description.trim();
+   body.id = todoNextId++;
+
+   todos.push(body)
+    
+   console.log('description: ' + body.description);
+   res.json(body);
+});
+
+app.listen(PORT, function() {
+    console.log('Express listen on port ' + PORT + '!')
+});
