fe1c91974b58e5de00c89981b7298e16597959c7
Mhu Htet <mhuhtet@gmail.com>
2015-12-08 17:32:30 +0630

    Prepare To Upload heroku

--- server.js
+++ server.js
@@ -13,11 +13,32 @@ app.get('/', function (req, res) {
     res.send('Todo API Root')
 });
 
+//GET /todo
+//app.get('/todos', function(req, res){
+//   res.json(todos) 
+//});
+
+//GET /todd?completed=true
 app.get('/todos', function(req, res){
-   res.json(todos) 
-});
+    var queryParams = req.query;
+    var fliteredTodos = todos;
+    
+    if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'true'){
+        fliteredTodos = _.where(fliteredTodos, {completed: true});
+    }else if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'false'){
+        fliteredTodos = _.where(fliteredTodos, {completed: false});
+    }
+    
+    if(queryParams.hasOwnProperty('q') && queryParams.q.length > 0 ){
+        fliteredTodos = _.filter(fliteredTodos, function (todo){
+            return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+        })
+    }
+    
+    res.json(fliteredTodos) 
+})
 
-
+//GET /todo/:id
 app.get('/todos/:id', function(req, res){
     var todoId = parseInt(req.params.id, 10);
     var matchedTodo = _.findWhere(todos, {id: todoId});
@@ -31,7 +52,7 @@ app.get('/todos/:id', function(req, res){
     
 });
 
-
+//POST /todos
 app.post('/todos', function (req, res) {
    var body = _.pick(req.body, 'description', 'completed');
     
@@ -45,6 +66,48 @@ app.post('/todos', function (req, res) {
     
    console.log('description: ' + body.description);
    res.json(body);
+});
+
+//DELETE /todos/:id
+
+app.delete('/todos/:id', function(req, res){
+    var todoID = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: todoID});
+    
+    if(!matchedTodo){
+        res.status(404).json({"error": "no todo found with that id"})}else {
+            todos = _.without(todos, matchedTodo)
+            res.json(matchedTodo)
+        }
+    });
+
+//PUT /todos/:id
+app.put('/todos/:id', function(req, res){
+    var todoID = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: todoID});
+    var body = _.pick(req.body, 'description', 'completed');
+    var validAttributes = {};
+    
+    if(!matchedTodo){
+        return res.status(404). send();
+    }
+    
+    if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+        validAttributes.completed = body.completed;
+    }else if(body.hasOwnProperty('completed')){
+        return res.status(400).send();
+    }
+    
+    if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0 ) {
+        validAttributes.description = body.description;
+    }else if (body.hasOwnProperty('description')){
+        return res.status(400).send();
+    }
+    
+    _.extend(matchedTodo, validAttributes);
+    
+    res.json(matchedTodo);
+    
 });
 
 app.listen(PORT, function() {
